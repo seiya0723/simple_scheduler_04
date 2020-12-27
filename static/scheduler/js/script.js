d6baad99d4a675abecb2b00150c234657cb9d310
Seiya Asahina <seiya@asahina>
2020-12-27 14:07:08 +0900

    search add

---
+++ static/scheduler/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load" , function (){
+
+    var config_dt   = {
+        enableTime: true,
+        dateFormat: "Y-m-d H:i",
+        "locale"  : "ja"
+    };
+
+    flatpickr("#dt",config_dt);
+
+});
+
